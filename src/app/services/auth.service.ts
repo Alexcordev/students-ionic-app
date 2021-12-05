0207e9329c8ba981d002639af1a17b697e1c8f41
Alexcordev <alexcordev@gmail.com>
2021-12-05 11:54:00 -0500

    Fixing datetime inputs

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,47 +5,39 @@ import { AlertController, NavController } from '@ionic/angular';
 import { BehaviorSubject } from 'rxjs';
 import { map } from 'rxjs/operators';
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class AuthService {
-  baseUrl: string = '/auth';
-  url: string = '/api';
+  baseUrl: string = "/auth";
+  url: string = "/api";
   private _user = new BehaviorSubject<User>(null);
 
-  constructor(
-    private http: HttpClient,
-    private alertCtrl: AlertController,
-    public navCtrl: NavController
-  ) {}
+
+  constructor(private http: HttpClient, private alertCtrl: AlertController, public navCtrl: NavController) {
+
+  }
 
   httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
 
   login(user: User) {
     console.log(user);
-    return this.http.post<User>(
-      `${this.baseUrl}/login`,
-      user,
-      this.httpOptions
-    );
-  }
+    return this.http.post<User>(`${this.baseUrl}/login`, user, this.httpOptions);
+}
 
-  register(user: User) {
-    console.log(user);
-    return this.http.post<User>(
-      `${this.baseUrl}/register`,
-      user,
-      this.httpOptions
-    );
-  }
+register(user: User) {
+  console.log(user);
+  return this.http.post<User>(`${this.baseUrl}/register`, user, this.httpOptions);
+}
 
-  getusername() {
-    return this.http.get(`${this.baseUrl}/username`);
-  }
+getusername() {
+  return this.http.get(`${this.baseUrl}/username`);
+}
+
 
   logout() {
-    return this.http.get(`${this.baseUrl}/logout`);
+      return this.http.get(`${this.baseUrl}/logout`);
   }
 
   showAlert(text: string, message: string) {
@@ -53,8 +45,9 @@ export class AuthService {
       .create({
         header: text,
         message: message,
-        buttons: ['Ok'],
+        buttons: ['Ok']
       })
-      .then((alertEl) => alertEl.present());
-  }
+      .then(alertEl => alertEl.present());
+    }
+
 }
